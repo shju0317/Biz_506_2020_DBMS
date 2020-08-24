e6e0f718e98f8f92e6e8d12cc207cd8880a38752
shju0317 <shju0317@naver.com>
2020-08-24 16:46:31 +0900

    200824

--- 오라클/USER1(2020-08-24-쇼핑몰).sql
+++ 오라클/USER1(2020-08-24-쇼핑몰).sql
@@ -12,7 +12,17 @@ COMMIT;
 
 SELECT RPAD('가', 10, 'P') FROM DUAL;
 
+/*
+table에서 1개의 칼럼을 PK를 설정할 수 없을 경우
+2개 이상의 칼럼을 묶어서 PK로 설정한다.
 
+하지만 아래 테이블에서 sc_num, sc_subject를 묶어서
+PK로 설정할 경우 sc_subject칼럼이 가변문자열인 관계로
+나중에 문제를 일으킬 수 있다.
+
+이러한 테이블에서는 데이터와 별도로 SEQ(ID)라는 칼럼을 만들고
+해당칼럼을 일련번호 등을 부여하고 PK로 만드는 것이 좋다.
+*/
 CREATE TABLE tbl_score(
     sc_num CHAR(5),
     sc_subject nVARCHAR2(20),
