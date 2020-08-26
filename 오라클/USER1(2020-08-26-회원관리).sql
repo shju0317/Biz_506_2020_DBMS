2658ad93ff1a0f931dbb27d416d2fa604127da8e
shju0317 <shju0317@naver.com>
2020-08-26 16:21:08 +0900

    200826

---
+++ 오라클/USER1(2020-08-26-회원관리).sql
@@ -0,0 +1,12 @@
+-- 여기는 USER1화면입니다.
+
+CREATE TABLE tbl_member(
+    M_USERID	VARCHAR2(50)	PRIMARY KEY,
+    M_PASSWORD	VARCHAR2(125)	NOT NULL,
+    M_NAME	    nVARCHAR2(30)	NOT NULL,
+    M_TEL	    VARCHAR(20),
+    M_ADDRESS	nVARCHAR2(255),		
+    M_ROLL	    VARCHAR2(20)	
+);
+
+ SELECT * FROM tbl_member;
