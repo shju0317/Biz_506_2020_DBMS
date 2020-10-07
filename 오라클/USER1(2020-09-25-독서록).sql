452fac18e38150c37e4eb384a217fc1146848c6b
shju0317 <shju0317@naver.com>
2020-10-07 13:28:35 +0900

    readBook

--- 오라클/USER1(2020-09-25-독서록).sql
+++ 오라클/USER1(2020-09-25-독서록).sql
@@ -12,8 +12,8 @@ CREATE TABLE tbl_member(
     
     -- ENABLE칼럼에 문자열 0 또는 1 이외의 값은 저장하지 말라
     -- CHECK 제약사항 등록
-    ENABLE	    CHAR(1)	DEFAULT '0' 
-                CONSTRAINT enable_veri CHECK(ENABLE= '0' OR ENABLE='1'),
+    ENABLED	    CHAR(1)	DEFAULT '0' 
+                CONSTRAINT enable_veri CHECK(ENABLED= '0' OR ENABLED='1'),
     AccountNonExpired	    CHAR(1),	
     AccountNonLocked	    CHAR(1),		
     CredentialsNonExpired	CHAR(1)		
---
+++ 오라클/USER1(2020-10-07).sql
@@ -0,0 +1,5 @@
+-- ReadBookV2
+
+SELECT * FROM tbl_member;
+DELETE FROM tbl_member WHERE m_userid = 'admin';
+COMMIT;
