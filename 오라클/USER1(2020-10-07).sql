e22137fe08c1c5b9d76fc60df8e4b3d31478f039
shju0317 <shju0317@naver.com>
2020-10-19 16:35:12 +0900

    bbs2020

--- 오라클/USER1(2020-10-07).sql
+++ 오라클/USER1(2020-10-07).sql
@@ -5,3 +5,13 @@ DELETE FROM tbl_member WHERE m_userid = 'admin';
 COMMIT;
 
 UPDATE tbl_member SET enabled = '1' WHERE m_userid = 'admin11';
+
+
+CREATE TABLESPACE tsDEPT
+DATAFILE 'C:/bizwork/workspace/oracle_data/tsDEPT.dbf'
+SIZE 1M AUTOEXTEND ON NEXT 500K;
+
+CREATE USER user1 IDENTIFIED BY 1234 
+DEFAULT TABLESPACE tsDEPT; 
+
+GRANT DBA TO user1;
